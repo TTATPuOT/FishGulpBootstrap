2bae53cb7c251b9d9d30e843ae085052eaa5cfc4
Anton <neverov12@gmail.com>
2020-03-19 15:43:25 +0500

    Added Sass support

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,10 @@ const { src, dest, watch } = require('gulp'),
 	browserSync = require('browser-sync'),
 	less = require('gulp-less'),
 	sourcemaps = require('gulp-sourcemaps'),
+	sass = require('gulp-sass'),
 	reload = browserSync.reload;
 
-const dev = !!(process.argv.find(i => i === '--r'));
+const dev = !(process.argv.find(i => i === '--r'));
 if (dev) {
 	console.log('Development GULP starts');
 } else{
@@ -34,21 +35,66 @@ imgTask = () => {
 	
 };
 
-cssTask = () => {
-	const sourceLess = Paths.setDirType('src').setFileType('css').setExtension('*.less').toString();
-	const source = Paths.setDirType('src').setFileType('css').setExtension('*.css').toString();
-	const build = Paths.setDirType('build').setExtension('').toString();
+stylesTask = (type = 'css') => {
+	console.log('StyleTask', type);
+	const build = Paths.setDirType('build').setFileType('css').setExtension('').toString();
 
-	if (dev)
-		src(sourceLess).pipe(sourcemaps.init()).pipe(less()).pipe(sourcemaps.write()).pipe(rigger()).pipe(cleanCSS({compatibility: 'ie8'})).pipe(dest(build)).pipe(reload({stream: true}));
-	else
-		src(sourceLess).pipe(less()).pipe(rigger()).pipe(cleanCSS({compatibility: 'ie8'})).pipe(dest(build)).pipe(reload({stream: true}));
+	if (type === 'less') {
+		const sourceLess = Paths.setDirType('src').setFileType('css').setExtension('*.less').toString();
 
-	if (dev) 
-		return src(source).pipe(rigger()).pipe(cleanCSS({compatibility: 'ie8'})).pipe(dest(build)).pipe(reload({stream: true}));
-	else
-		return src(source).pipe(rigger()).pipe(dest(build)).pipe(reload({stream: true}));
+		if (dev)
+			return src(sourceLess)
+				.pipe(sourcemaps.init())
+				.pipe(less())
+				.pipe(sourcemaps.write())
+				.pipe(rigger())
+				.pipe(cleanCSS({compatibility: 'ie8'}))
+				.pipe(dest(build))
+				.pipe(reload({stream: true}));
+		else
+			return src(sourceLess)
+				.pipe(less())
+				.pipe(rigger())
+				.pipe(cleanCSS({compatibility: 'ie8'}))
+				.pipe(dest(build))
+				.pipe(reload({stream: true}));
+	} else if (type === 'sass') {
+		const sourceSass = Paths.setDirType('src').setFileType('css').setExtension('*.sass').toString();
+
+		if (dev)
+			return src(sourceSass)
+				.pipe(sourcemaps.init())
+				.pipe(sass().on('error', sass.logError))
+				.pipe(rigger())
+				.pipe(cleanCSS({compatibility: 'ie8'}))
+				.pipe(sourcemaps.write())
+				.pipe(dest(build))
+				.pipe(reload({stream: true}));
+		else
+			return src(sourceSass)
+				.pipe(sass().on('error', sass.logError))
+				.pipe(rigger())
+				.pipe(cleanCSS({compatibility: 'ie8'}))
+				.pipe(dest(build))
+				.pipe(reload({stream: true}));
+	} else{
+		const source = Paths.setDirType('src').setFileType('css').setExtension('*.css').toString();
+
+		if (dev) 
+			return src(source)
+				.pipe(rigger())
+				.pipe(cleanCSS({compatibility: 'ie8'}))
+				.pipe(dest(build))
+				.pipe(reload({stream: true}));
+		else
+			return src(source)
+				.pipe(rigger())
+				.pipe(dest(build))
+				.pipe(reload({stream: true}));
+	}
 };
+lessTask = () => stylesTask('less');
+sassTask = () => stylesTask('sass');
 
 jsTask = () => {
 	const source = Paths.setDirType('src').setFileType('js').setExtension('*.js').toString();
@@ -84,7 +130,9 @@ fontsTask = () => {
 
 workAll = () => {
 	imgTask();
-	cssTask();
+	stylesTask();
+	lessTask();
+	sassTask();
 	jsTask();
 	fontsTask();
 	htmlTask();
@@ -106,7 +154,9 @@ defaultTask = cb => {
 	Paths.setDirType('src');
 
 	watch(Paths.setFileType('img').setExtension('*').toString(), imgTask);
-	watch(Paths.setFileType('css').setExtension('*.*').toString(), cssTask);
+	watch(Paths.setFileType('css').setExtension('*.css').toString(), stylesTask);
+	watch(Paths.setFileType('css').setExtension('*.less').toString(), lessTask);
+	watch(Paths.setFileType('css').setExtension('*.sass').toString(), sassTask);
 	watch(Paths.setFileType('js').setExtension('*.js').toString(), jsTask);
 	watch(Paths.setFileType('fonts').setExtension('*').toString(), fontsTask);
 	watch(Paths.setFileType('').setExtension('*.html').toString(), htmlTask);
